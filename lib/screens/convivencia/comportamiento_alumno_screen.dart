f9e035e0cf62a7be8aceb768db2c07ff8b5f3951
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-12-01 14:29:18 +0100

    Se añaden campos para la interfaz

--- lib/screens/convivencia/comportamiento_alumno_screen.dart
+++ lib/screens/convivencia/comportamiento_alumno_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iseneca/widgets/widgets.dart';
 
 class ComportamientoAlumnoScreen extends StatelessWidget {
   const ComportamientoAlumnoScreen({super.key});
@@ -22,7 +23,49 @@ class ComportamientoAlumnoScreen extends StatelessWidget {
                 height: screenSize.height * 0.05,
               ),
               Container(
-                height: screenSize.height * 0.2,
+                width: screenSize.width * 0.9,
+                height: screenSize.height * 0.15,
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(40)),
+                    color: theme.secondaryHeaderColor),
+                child: Column(
+                  children: [
+                    Container(
+                        width: double.infinity,
+                        height: 40,
+                        child: Center(
+                            child: Text(
+                          "HORA",
+                          style: TextStyle(
+                              color: theme.secondaryHeaderColor,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic),
+                        )),
+                        decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.vertical(
+                                top: Radius.circular(40)),
+                            color: theme.primaryColor)),
+                    createVerticalSeparator(0.035, context),
+                    Container(
+                      decoration: BoxDecoration(color: theme.primaryColor,borderRadius: BorderRadius.all(Radius.circular(40))),
+                      width: screenSize.width*0.15,
+                        child: FilledButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(Icons.calendar_month),
+                          label: const Text(
+                            "Fecha",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          style: const ButtonStyle(
+                            iconSize: MaterialStatePropertyAll(35),
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+              Container(
                 width: screenSize.width * 0.9,
                 decoration: BoxDecoration(
                     borderRadius: const BorderRadius.all(Radius.circular(40)),
@@ -42,16 +85,55 @@ class ComportamientoAlumnoScreen extends StatelessWidget {
                               fontStyle: FontStyle.italic),
                         )),
                         decoration: BoxDecoration(
-                            borderRadius:
-                                const BorderRadius.vertical(top: Radius.circular(40)),
+                            borderRadius: const BorderRadius.vertical(
+                                top: Radius.circular(40)),
                             color: theme.primaryColor)),
+                    const ExpansionTile(
+                      title: Text(
+                        "Horas",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      children: [
+                        //TODO: Colocar las horas mediante servicio
+                        Text(
+                          "Colocar horas en RadioListTile",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        Text(
+                          "Colocar horas en RadioListTile",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        Text(
+                          "Colocar horas en RadioListTile",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        Text(
+                          "Colocar horas en RadioListTile",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        Text(
+                          "Colocar horas en RadioListTile",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        Text(
+                          "Colocar horas en RadioListTile",
+                          style: TextStyle(fontSize: 32),
+                        )
+                      ],
+                    )
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  createVerticalSeparator(cuantity, context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * cuantity,
     );
   }
 }
